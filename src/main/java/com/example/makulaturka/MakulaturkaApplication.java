641cd8f856892d8b373e10da117ca49f77f27d61
Shadowproof <kubac400@gmail.com>
2024-01-11 07:57:34 +0100

    Initial commit

---
+++ src/main/java/com/example/makulaturka/MakulaturkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.makulaturka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakulaturkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakulaturkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makulaturka/MakulaturkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makulaturka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakulaturkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
