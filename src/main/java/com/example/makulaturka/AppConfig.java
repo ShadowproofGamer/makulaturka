fcab48e6cf95c5e779fda3e652d3692733316fd0
ShadowproofGamer <57487079+ShadowproofGamer@users.noreply.github.com>
2024-01-23 20:38:00 +0100

    Logic + DB działa (TODO Controller + frontend)

--- src/main/java/com/example/makulaturka/AppConfig.java
+++ src/main/java/com/example/makulaturka/AppConfig.java
@@ -2,7 +2,9 @@ package com.example.makulaturka;
 
 import com.example.makulaturka.account.Account;
 import com.example.makulaturka.account.AccountRepository;
+import com.example.makulaturka.address.Address;
 import com.example.makulaturka.address.AddressRepository;
+import com.example.makulaturka.client.Client;
 import com.example.makulaturka.client.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -25,8 +27,28 @@ public class AppConfig {
                     "JakCeb",
                     "password"
             );
+            Address ad1 = new Address(
+                    1L,
+                    "Inowroclawska",
+                    "6",
+                    "12",
+                    "53-653",
+                    "Wroclaw"
+            );
+            Client c1 = new Client(
+                    1L,
+                    a1,
+                    ad1,
+                    1000L
+            );
             accountRepository.saveAll(
                     List.of(a1)
+            );
+            addressRepository.saveAll(
+                    List.of(ad1)
+            );
+            clientRepository.saveAll(
+                    List.of(c1)
             );
         };
     }
